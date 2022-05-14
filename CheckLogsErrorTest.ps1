ad5a422ae855474c44b211c8e337814b21c16630
DCMetroDevOp <77302625+DCMetroDevOp@users.noreply.github.com>
2022-05-14 07:30:59 -0400

    Add files via upload

---
+++ CheckLogsErrorTest.ps1
@@ -0,0 +1,5 @@
+﻿ $LogDate = Get-Date -UFormat %Y%m%d
+ $LogName = 'SyncHardwareAssets_'
+ $Log = "$LogName$LogDate*"
+ #$Results = 
+ Get-ChildItem -Path "\ServerName\Share\$Log" -Recurse | Select-String "Error|Warning"
---
+++ SplitStrings.ps1
@@ -0,0 +1,11 @@
+﻿$o = '---'
+##$o = 'aalleyne@aoc.gov 41EAD8E1-A5C8-4F96-8E91-0C7CE18D7B0C'
+$Ob = IF ($o -like "*@domain.com*") {($F = $o -replace "\@domain.com.+") 
+$User = Get-Aduser $F}
+ElSE {
+($F = $o -replace '\s+', ', ')
+$Query = "SELECT 
+      [Full_User_Name0]
+      FROM [CM_HQ2].[dbo].[v_R_User]
+  WHERE [Full_User_Name0] Like '$F'"
+$QueryResult = Invoke-Sqlcmd -Query $Query -ServerInstance 'DBServer' -Database 'DBName'}

ab1e7f7964b9482122a456d3793308fe028fa3f1
DCMetroDevOp <77302625+DCMetroDevOp@users.noreply.github.com>
2022-05-14 07:21:09 -0400

    Add files via upload

---
+++ Cunks.ps1
@@ -0,0 +1,17 @@
+﻿$custIDs = Get-MicrosoftPartnerCustomers
+$chunks = [System.Collections.ArrayList]::new()
+for ($i = 0; $i -lt $custIDs.Count; $i += 100) {
+    if (($custIDs.Count - $i) -gt 99  ) {
+        $chunks.add($custIDs[$i..($i + 99)])
+    }
+    else {
+        $chunks.add($custIDs[$i..($custIDs.Count - 1)])
+    }
+}
+$today = [datetime]::Today.ToString("yyyy-MM-dd")
+$count = 1
+foreach ($chunk in $chunks) {
+    $path = "c:\Reports\$today Chunk $count of $($chunks.Count).csv"
+    $chunk | Export-Csv $path -Append
+    $count++
+}
